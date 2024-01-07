cee9a97d43e09f77f72faf3f97565e85b9c012dc
nikozka15 <vshyrshova@gmail.com>
2024-01-07 16:22:52 +0000

    fixed problem with message bundle

--- src/main/java/com/nikozka/app/configuration/SecurityConfiguration.java
+++ src/main/java/com/nikozka/app/configuration/SecurityConfiguration.java
@@ -32,7 +32,7 @@ public class SecurityConfiguration {
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/swagger-ui/**", "v3/api-docs/**", "/users", "/users/authenticate")
+                        .requestMatchers("/swagger-ui.html", "v3/api-docs/**", "/users", "/users/authenticate")
                         .permitAll()
                         .anyRequest()
                         .authenticated())
--- src/main/java/com/nikozka/app/servises/TaskService.java
+++ src/main/java/com/nikozka/app/servises/TaskService.java
@@ -68,7 +68,8 @@ public class TaskService {
         TaskStatusValidator updater = factory.getValidator(previousStatus);
 
         if (!updater.isTaskStatusValid(task, newStatus)) {
-            throw new InvalidStateException(messageSource.getMessage("task.invalid.state.transition", new Object[]{taskId}, LocaleContextHolder.getLocale()));
+            throw new InvalidStateException(messageSource.getMessage(
+                    "task.invalid.state.transition", new Object[]{taskId}, LocaleContextHolder.getLocale()));
         }
         task.setStatus(newStatus);
         taskRepository.save(task);
